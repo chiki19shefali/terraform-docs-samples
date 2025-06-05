c72282bbb43488258a41a98b08d40de4ce1c642f
chiki19shefali <chiki19shefali@gmail.com>
2025-06-05 19:22:39 +0000

    loc

--- gke/quickstart/autopilot/cluster.tf
+++ gke/quickstart/autopilot/cluster.tf
@@ -26,7 +26,7 @@ resource "google_compute_subnetwork" "default" {
   name = "example-subnetwork"
 
   ip_cidr_range = "10.0.0.0/16"
-  region        = "us-central1"
+  region        = "northamerica-northeast2-a"
 
   stack_type       = "IPV4_IPV6"
   ipv6_access_type = "INTERNAL" # Change to "EXTERNAL" if creating an external loadbalancer
@@ -46,7 +46,7 @@ resource "google_compute_subnetwork" "default" {
 resource "google_container_cluster" "default" {
   name = "example-autopilot-cluster"
 
-  location                 = "us-central1"
+  location                 = "northamerica-northeast2-a"
   enable_autopilot         = true
   enable_l4_ilb_subsetting = true
 
